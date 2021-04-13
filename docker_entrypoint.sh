07c5f43154fd6366a4bdbed46a0be8c5f74feb1e
沉冰浮水 <wdssmq@qq.com>
2021-04-13 16:04:39 +0800

    拆分步骤；

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
+cd /app
 ## Download Z-BlogPHP
 if ! [ -e /app/index.php ]; then
 	echo Downloading Z-BlogPHP...
-	cd /app
 	wget https://update.zblogcn.com/zip/Z-BlogPHP_1_6_7_2160_Valyria.zip
-	unzip Z-BlogPHP_1_6_7_2160_Valyria.zip
-	rm Z-BlogPHP_1_6_7_2160_Valyria.zip
+	echo Unpacking Z-BlogPHP...
+	unzip -oq Z-BlogPHP_1_6_7_2160_Valyria.zip
 	## Modify permissions
 	chown -R www-data:www-data /app
+fi
+if ! [ -e /app/zb_users/c_option.php ]; then
+	echo Installing Z-BlogPHP...
 	php /app/install-docker.php
+fi
+if [ -e /app/zb_users/c_option.php ]; then
+	rm Z-BlogPHP_1_6_7_2160_Valyria.zip
 	rm /app/install-docker.php
 	rm -rf /app/zb_install
 fi
